73b8ed3e45bc8500e8463b9f4c09041c1f4d7024
Ujjwal8271 <ujjwal.it.20183048@gmail.com>
2023-10-07 14:45:29 +0530

    assignment1

---
+++ assignment1/average/src/average/average.java
@@ -0,0 +1,22 @@
+package average;
+
+import java.util.Scanner;
+
+public class average {
+	public static void main(String[] args)
+	{    
+		 double x;
+		 double y;
+		 Scanner sc = new Scanner(System.in); 
+		  System.out.println("Enter Two Number ");
+		 x=sc.nextDouble();
+		 x=sc.nextDouble();
+		 if()
+		 {
+			 
+		 }
+		
+		   
+	}
+
+}
