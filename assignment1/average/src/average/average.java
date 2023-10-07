7ecdb2306c0a2e00587f39597756d7372febd9e0
KD3-80146 <98707983+KD3-80146@users.noreply.github.com>
2023-10-08 00:36:24 +0530

    Update average.java

--- assignment1/average/src/average/average.java
+++ assignment1/average/src/average/average.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class average {
 	public static void main(String[] args)
-	{    
-		 double x;
-		 double y;
-		 Scanner sc = new Scanner(System.in); 
-		  System.out.println("Enter Two Number ");
-		 x=sc.nextDouble();
-		 x=sc.nextDouble();
-		 if()
-		 {
-			 
-		 }
+	Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the first double value: ");
+		if (scanner.hasNextDouble()) {
+			double num1 = scanner.nextDouble();
+			scanner.nextLine(); 
+
+			System.out.print("Enter the second double value: ");
+			if (scanner.hasNextDouble()) {
+				double num2 = scanner.nextDouble();
+				
+				double average = (num1 + num2) / 2;
+				System.out.println("The average is: " + average);
+			} else {
+				System.out.println("Error: Second input is not a double value.");
+			}
+		} else {
+			System.out.println("Error: First input is not a double value.");
+		}
+
 		
-		   
 	}
 
 }
